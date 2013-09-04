bab8754edc950c76b839e48c958389d2430461c6
Mattias Petter Johansson <mpj@spotify.com>
2013-09-04 19:34:56 +0200

    Started working on basic, messy app tests

--- app.js
+++ app.js
@@ -1,20 +1,95 @@
-function newApp() {
-  
-  var api = {
-    overview: function(next) {
-      var days = new Array(18)
-      days.unshift({
-        heading: 'WED 04'
-      })
-      days.unshift({
-        heading: 'TUE 03'
-      })
-      days.push({
-        heading: 'TUE 24'
-      })
+function newApp(mongo, time, facebook, session) {
 
-      next(null, {
-        days: days
+  var displayDays = 21
+
+  function weekDayText(date) { 
+    switch(date.getDay()) {
+      case 0: return 'SUN'
+      case 1: return 'MON'
+      case 2: return 'TUE'
+      case 3: return 'WED'
+      case 4: return 'THU'
+      case 5: return 'FRI'
+      case 6: return 'SAT'
+    }
+  }
+  function dateText(date) {
+    var str = '' + date.getDate()
+    return str.length === 1 ? '0' + str : str
+  }
+
+  function padded(num) {
+    return num < 10 ? '0' + num : '' + num
+  }
+
+  function storeDate(date) {
+    return  date.getFullYear() 
+            + '-' + 
+            padded(date.getMonth() + 1)
+            + '-' + 
+            padded(date.getDate())
+  }
+
+  var api = {
+
+    overview: function(next) {
+      var days = []
+      
+      facebook.getUserAndFriends(session.get('facebook_token'), function(err, userAndFriends) {
+        var persons = {}
+        persons[userAndFriends.id] = {
+          first_name: userAndFriends.first_name,
+          picture:    userAndFriends.picture
+        }
+         
+        mongo.collection('availabilities')
+        .find({ fb_user_id: { $in: Object.keys(persons)} })
+        .toArray(function(err, avails) {
+
+          // Days with headings
+          var timeCursor = time.get()
+          for(var i = 0; i < displayDays; i++) {
+            var day = {
+              heading: weekDayText(timeCursor) + ' ' + dateText(timeCursor),
+              segments: {
+                daytime: {
+                  heading: "Daytime"
+                },
+                evening: {
+                  heading: "Evening"
+                }
+              }
+            }
+
+            var matchesDay = avails.filter(function(a) {
+              return a.date === storeDate(timeCursor) })
+            var matches = {
+              daytime: matchesDay.filter(function(a) { return a.segment === 'daytime' }),
+              evening: matchesDay.filter(function(a) { return a.segment === 'evening' })
+            }
+
+            day.segments.daytime.persons = matches.daytime.map(function(a) {
+              return {
+                imageSrc: persons[a.fb_user_id].picture
+              }
+            })
+            day.segments.evening.persons = matches.evening.map(function(a) {
+              return {
+                imageSrc: persons[a.fb_user_id].picture
+              }
+            })
+
+            days.push(day) 
+
+            timeCursor.setDate(timeCursor.getDate() + 1)
+            
+          }
+
+
+          next(null, {
+            days: days
+          })
+        })
       })
     }
   }
--- test/authentication.js
+++ test/authentication.js
@@ -8,65 +8,120 @@ describe('setup', function() {
 	var database;
 	var app;
 	var time;
+	var facebook;
+	var session;
 
 	beforeEach(function(done) {
 		withDatabase(function(db) {
 			database = db
 			time = newTime()
-			app = newApp(database, time)
+			facebook = { getUserAndFriends: function(err, next) {
+				next(null, {
+					id: '12345'
+				})
+			}}
+			session = { get: function() { return 'dummy-token' }}
+			app = newApp(database, time, facebook, session)
+			database.collection('availabilities').drop()
 			done()
 		})
 	})
 
-	describe('we have a single availability', function() {
-		beforeEach(function(done) {
-			database.collection('availabilities').insert({
-				fb_user_id: '712821789127',
-				availability: 'free',
-				date: '2013-01-03',
-				segment: 'evening'
-			}, function(err, result) {
-				done()
-			})
+
+	describe('date is 2013-09-03', function() {
+		beforeEach(function() {
+			time.override(new Date('2013-09-03'))
 		})
 
-		describe('date is 2013-09-03', function() {
-			beforeEach(function() {
-				time.override(new Date('2013-09-03'))
+		describe('we load overview', function() {
+			var overview;
+			beforeEach(function(done) {
+				app.overview(function(err, result) {
+					overview = result;
+					done()
+				})
+			})
+
+			it('should yield 21 days', function() {
+				overview.days.length.should.equal(21)
+			})
+
+			it('yields 2013-09-03 as first day', function() {
+				overview.days[0].heading.should.equal('TUE 03')
+			})
+
+			it('yields 2013-09-04 as second day', function() {
+				overview.days[1].heading.should.equal('WED 04')
+			})
+
+			it('yields 2013-09-05 as third day', function() {
+				overview.days[2].heading.should.equal('THU 05')
+			})
+
+			it('yields 2013-09-06 as fourth day', function() {
+				overview.days[3].heading.should.equal('FRI 06')
+			})
+
+			it('yields 2013-09-07 as fifth day', function() {
+				overview.days[4].heading.should.equal('SAT 07')
+			})
+
+			it('yields 2013-09-08 as sixth day', function() {
+				overview.days[5].heading.should.equal('SUN 08')
+			})
+
+			it('yields 2013-09-23 as 21st day', function() {
+				overview.days[20].heading.should.equal('MON 23')
+			})
+
+		})
+
+		describe('we have a single availability', function() {
+			beforeEach(function(done) {
+				database.collection('availabilities').insert({
+					fb_user_id: '712821789127',
+					availability: 'free',
+					date: '2013-09-08',
+					segment: 'evening'
+				}, function(err, result) {
+					done()
+				})
+
+				session.get = function fakeSessionGet(key) {
+					if (key === 'facebook_token') return '!secret'
+					throw new Error('Tried to get ' + key)
+				}
+
+				facebook.getUserAndFriends = function fakeGetUser(token, next) {
+					if (token != '!secret') throw new Error('token was incorrect')
+					next(null, {
+						id: '712821789127',
+						first_name: 'Mattias',
+						picture: 'http://facebook.com/best_people_images/mattias.jpg'
+					})
+				}
+
 			})
 
 			describe('we load overview', function() {
 				var overview;
-				beforeEach(function() {
+				var person;
+				beforeEach(function(done) {
 					app.overview(function(err, result) {
 						overview = result;
+						person = overview.days[5].segments.evening.persons[0];
+						done()
 					})
 				})
 
-				it('should yield 21 days', function() {
-					overview.days.length.should.equal(21)
+				it('displays the user picture', function() {
+					person.imageSrc.should.equal('http://facebook.com/best_people_images/mattias.jpg')
 				})
-
-				it('yields 2013-09-03 as first day', function() {
-					overview.days[0].heading.should.equal('TUE 03')
-				})
-
-				it('yields 2013-09-04 as second day', function() {
-					overview.days[1].heading.should.equal('WED 04')
-				})
-
-				it('yields 2013-09-24 as 21st day', function() {
-					overview.days[20].heading.should.equal('TUE 24')
-				})
-
-
 
 			})
-		})
 
-		
+		})
 	})
-	
 })
 
 
